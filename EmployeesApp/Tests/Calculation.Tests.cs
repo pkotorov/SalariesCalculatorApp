fb170500951ac411a5080f412c3f3c11a3464b76
Petar Kotorov <petar.kotorov@yahoo.de>
2022-02-22 08:17:45 +0100

    Adding Unit Tests

--- EmployeesApp/Tests/Calculation.Tests.cs
+++ EmployeesApp/Tests/Calculation.Tests.cs
@@ -1,9 +1,5 @@
 ﻿using EmployeesApp.Data.Helpers;
 using EmployeesApp.Data.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace EmployeesApp.Tests
@@ -11,7 +7,7 @@ namespace EmployeesApp.Tests
     public class Calculation
     {
         [Fact]
-        public void ShouldNotCalculateTaxesWhenGrossPayIsLessThanOneThousand()
+        public void Should_Not_Calculate_Taxes_When_GrossPay_Is_Less_Than_One_Thousand()
         {
             var employee = new EmployeeViewModel
             {
@@ -24,7 +20,7 @@ namespace EmployeesApp.Tests
         }
 
         [Fact]
-        public void ShouldNotCalculateTaxesWhenGrossPayIsEqualToOneThousand()
+        public void Should_Not_Calculate_Taxes_When_GrossPay_Is_Equal_To_One_Thousand()
         {
             var employee = new EmployeeViewModel
             {
@@ -34,6 +30,292 @@ namespace EmployeesApp.Tests
             employee = Calculator.CalculateNetPayAndTaxes(employee);
 
             Assert.Equal(0, employee.TaxesTotal);
+        }
+
+        [Fact]
+        public void Should_Not_Calculate_Taxes_When_GrossPay_Is_Equal_To_Zero()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 0
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(0, employee.TaxesTotal);
+        }
+
+        [Fact]
+        public void Should_Calculate_Correctly_Taxes_When_GrossPay_Is_Between_1001_And_2999()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 1500
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(125, employee.TaxesTotal);
+        }
+
+        [Fact]
+        public void Should_Calculate_Correctly_Taxes_When_GrossPay_Is_Equal_To_Three_Tousand()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 3000
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(500, employee.TaxesTotal);
+        }
+
+        [Fact]
+        public void Should_Calculate_Correctly_Taxes_When_GrossPay_Is_More_Than_Three_Tousand()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 3500
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(550, employee.TaxesTotal);
+        }
+
+        [Fact]
+        public void Should_Not_Calculate_IncomeTax_When_GrossPay_Is_Less_Than_One_Thousand()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 950
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(0, employee.IncomeTax);
+        }
+
+        [Fact]
+        public void Should_Not_Calculate_IncomeTax_When_GrossPay_Is_Equal_To_One_Thousand()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 1000
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(0, employee.IncomeTax);
+        }
+
+        [Fact]
+        public void Should_Not_Calculate_IncomeTax_When_GrossPay_Is_Equal_To_Zero()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 0
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(0, employee.IncomeTax);
+        }
+
+        [Fact]
+        public void Should_Calculate_Correctly_IncomeTax_When_GrossPay_Is_Between_1001_And_2999()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 1500
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(50, employee.IncomeTax);
+        }
+
+        [Fact]
+        public void Should_Calculate_Correctly_IncomeTax_When_GrossPay_Is_Equal_To_Three_Tousand()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 3000
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(200, employee.IncomeTax);
+        }
+
+        [Fact]
+        public void Should_Calculate_Correctly_IncomeTax_When_GrossPay_Is_More_Than_Three_Tousand()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 3500
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(250, employee.IncomeTax);
+        }
+
+        [Fact]
+        public void Should_Not_Calculate_Health_And_Social_Insurance_Taxes_When_GrossPay_Is_Less_Than_One_Thousand()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 950
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(0, employee.HealthAndSocialInsuranceTax);
+        }
+
+        [Fact]
+        public void Should_Not_Calculate_Health_And_Social_Insurance_Taxes_When_GrossPay_Is_Equal_To_One_Thousand()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 1000
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(0, employee.HealthAndSocialInsuranceTax);
+        }
+
+        [Fact]
+        public void Should_Not_Calculate_Health_And_Social_Insurance_Taxes_When_GrossPay_Is_Equal_To_Zero()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 0
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(0, employee.HealthAndSocialInsuranceTax);
+        }
+
+        [Fact]
+        public void Should_Calculate_Correctly_Health_And_Social_Insurance_Taxes_When_GrossPay_Is_Between_1001_And_2999()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 1500
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(75, employee.HealthAndSocialInsuranceTax);
+        }
+
+        [Fact]
+        public void Should_Calculate_Correctly_Health_And_Social_Insurance_Taxes_When_GrossPay_Is_Equal_To_Three_Tousand()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 3000
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(300, employee.HealthAndSocialInsuranceTax);
+        }
+
+        [Fact]
+        public void Should_Calculate_Correctly_Health_And_Social_Insurance_Taxes_When_GrossPay_Is_More_Than_Three_Tousand()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 3500
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(300, employee.HealthAndSocialInsuranceTax);
+        }
+
+        [Fact]
+        public void Should_Calculate_NetPay_Correctly_When_GrossPay_Is_Less_Than_One_Thousand()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 950
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(950, employee.NetPay);
+        }
+
+        [Fact]
+        public void Should_Calculate_NetPay_Correctly_When_GrossPay_Is_Equal_To_One_Thousand()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 1000
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(1000, employee.NetPay);
+        }
+
+        [Fact]
+        public void Should_Calculate_NetPay_Correctly_When_GrossPay_Is_Equal_To_Zero()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 0
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(0, employee.NetPay);
+        }
+
+        [Fact]
+        public void Should_Calculate_NetPay_Correctly_When_GrossPay_Is_Between_1001_And_2999()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 1500
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(1375, employee.NetPay);
+        }
+
+        [Fact]
+        public void Should_Calculate_Correctly_NetPay_When_GrossPay_Is_Equal_To_Three_Tousand()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 3000
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(2500, employee.NetPay);
+        }
+
+        [Fact]
+        public void Should_Calculate_Correctly_NetPay_When_GrossPay_Is_More_Than_Three_Tousand()
+        {
+            var employee = new EmployeeViewModel
+            {
+                GrossPay = 3500
+            };
+
+            employee = Calculator.CalculateNetPayAndTaxes(employee);
+
+            Assert.Equal(2950, employee.NetPay);
         }
     }
 }
